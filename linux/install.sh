3ac85b018ce637bd62739e8690eb9c1124fa9fcc
Wsine <weizhy3@hotmail.com>
2017-12-20 13:55:25 +0800

    update download info

--- linux/install.sh
+++ linux/install.sh
@@ -2,54 +2,75 @@
 
 # set input from and output to
 GITHUB_DIR="https://raw.githubusercontent.com/Wsine/Backup/master/linux"
-OUTPUT_DIR="~"
+OUTPUT_DIR="$HOME"
 
-# check compile environment
-command -v wget || {
-    echo "-- ERROR: no wget executable is found."
-    exit 1
-}
-
-command -v sed || {
-    echo "-- ERROR: no sed executable is found."
-    exit 1
-}
-
-# check path
-if [ ! -d "$OUTPUT_DIR" ]; then
-    mkdir -p "$OUTPUT_DIR"
-fi
-
-# install ~/.inputrc
-wget $GITHUB_DIR/.inputrc -O $OUTPUT_DIR/.inputrc
-
-# install ~/.bashrc
-if command -v bash > /dev/null; then
-    wget $GITHUB_DIR/.bashrc -O $OUTPUT_DIR/.bashrc
-fi
-
-# install ~/.bash_aliases
-if command -v bash > /dev/null; then
-    wget $GITHUB_DIR/.bash_aliases -O $OUTPUT_DIR/.bash_aliases
-fi
-
-# install ~/.zshrc
-if command -v zsh > /dev/null; then
-    wget $GITHUB_DIR/.zshrc.example -O $OUTPUT_DIR/.zshrc
-    sed -i 's/user_name/'$USER'/g' $OUTPUT_DIR/.zshrc
-fi
-
-# install ~/.tmux.conf
-if command -v tmux > /dev/null; then
-    wget $GITHUB_DIR/.tmux.conf -O $OUTPUT_DIR/.tmux.conf
-fi
-
-# install ~/.vimrc
-if command -v vim > /dev/null; then
-    wget $GITHUB_DIR/.vimrc -O $OUTPUT_DIR/.vimrc
-    if [ ! -d "$OUTPUT_DIR/.vim/colors" ]; then
-        mkdir -p $OUTPUT_DIR/.vim/colors
+function info() {
+    local filename=$1
+    if [ $? -eq 0 ]; then
+        echo "download success ==> $filename"
     fi
-    wget $GITHUB_DIR/desert.vim -O $OUTPUT_DIR/.vim/colors/desert.vim
-fi
+}
+
+function error() {
+    local execname=$1
+    echo "-- ERROR: no $execname executable is found."
+    exit 1
+}
+
+function check_env() {
+    local execname=$1
+    command -v $execname &> /dev/null || {
+        error $execname
+    }
+}
+
+function check_path() {
+    local path=$1
+    if [ ! -d "$path" ]; then
+        mkdir -p $path
+    fi
+}
+
+function download() {
+    local filename=$1
+    local specpath=$2
+    wget -q $GITHUB_DIR/$filename -O $OUTPUT_DIR/$specpath/$filename
+    info $filename
+}
+
+function check_for_download() {
+    local cmd=$1
+    local filename=$2
+    local specpath=$3
+    local replace=$4
+    if command -v $cmd > /dev/null; then
+        if [ -n "$specpath" ]; then
+            check_path "$OUTPUT_DIR/$specpath"
+        fi
+        download $filename $specpath
+        if [ -n "$replace" ]; then
+            sed -i "$replace" $OUTPUT_DIR/$specpath/$filename
+        fi
+    fi
+}
+
+function main() {
+    # check compile environment
+    check_env "wget"
+    check_env "sed"
+
+    # check path
+    check_path "$OUTPUT_DIR"
+
+    # download file if needed
+    check_for_download "bash" ".inputrc"
+    check_for_download "bash" ".bashrc"
+    check_for_download "bash" ".bash_aliases"
+    check_for_download "tmux" ".tmux.conf"
+    check_for_download "vim" ".vimrc"
+    check_for_download "vim" "desert.vim" ".vim/colors"
+    check_for_download "zsh" ".zshrc" "" "s/user_name/"$USER"/g"
+}
+
+main && echo "all downloads success."
 
